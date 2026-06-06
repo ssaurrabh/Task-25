1163b742ad05b76384cd190a1cfe5f92cd3fc297
Saurabh Sharma <83503309+ssaurrabh@users.noreply.github.com>
2026-06-06 17:42:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function show(){
+    let mymenu = document.getElementById("menu")
+    mymenu.style.color= 'black'
+    mymenu.style.width = '65%'
+    mymenu.style.margin = 'auto'
+    mymenu.classList.toggle("block")
+    mymenu.classList.toggle("hidden");
+}
